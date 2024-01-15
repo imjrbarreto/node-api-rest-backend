45cb7a1f5538e7020bbe988c823770a5adc1e71e
imjrbarreto <imjrbarreto@gmail.com>
2024-01-15 09:49:52 -0300

    Instalacon de Express y primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('my app')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hola mi server en express')
+})
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+})
+
