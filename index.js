a8df5bfb1959135ba503e53bc4595813a1eae51a
imjrbarreto <imjrbarreto@gmail.com>
2024-01-15 14:53:27 -0300

    GET: parámetros query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { faker } = require('@faker-js/faker')
+
 const app = express()
 const port = 3000
 
@@ -11,16 +13,17 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 2000
-    }
-  ])
+  const { size } = req.query
+  const limit = size || 10
+  const products = []
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url()
+    })
+  }
+  res.json(products)
 })
 
 app.get('/products/:id', (req, res) => {
@@ -30,6 +33,22 @@ app.get('/products/:id', (req, res) => {
     name: 'Product 2',
     price: 2000
   })
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('yo soy un product filter...')
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parametros')
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
