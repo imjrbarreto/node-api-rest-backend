ab4d7ed5eecd7b53e5ace9adbd511094a8f4ae60
imjrbarreto <imjrbarreto@gmail.com>
2024-01-19 20:30:29 -0300

    CORS: errores solucionados

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const routerApi = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-// const whiteList = ['http://localhost:5500', 'https://myapp.co']
-// const options = {
-//   origin: (origin, callback) => {
-//     if (whiteList.includes(origin)) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('No permitido'))
-//     }
-//   }
-// }
-// app.use(cors(options))
+const whitelist = ['http://localhost:5500', 'https://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
 
 // app.use(cors())
 
-app.use(cors({ origin: 'http://localhost:5500/'}))
+// app.use(cors({ origin: 'http://localhost:5500/'}))
 
 app.get('/', (req, res) => {
   res.send('hola mi server en express')
 })
 
 routerApi(app)
+app.use(cors(options))
 
 app.use(logErrors)
 app.use(boomErrorHandler)
